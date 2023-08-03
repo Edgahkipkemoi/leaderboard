d427532b8d91eb30ebfc746af502aa4d49930631
unknown <kipkemoiedgah@gmail.com>
2023-08-03 19:33:24 +0300

    fixed linters

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+
+  devServer: {
+    static: './dist',
   },
-  module: {
-    rules: [
-    ],
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Output Management',
       template: './src/index.html',
     }),
   ],
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: 'single',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
